ed5f291b27249859c6aac58e35c7b7587f77d2d9
YaushevKamil <kyaush@mail.ru>
2019-10-11 01:57:56 +0300

    Scripted auto-commit on change (2019-10-11 01:57:56) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
@@ -4,5 +4,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWrightableComparable, Text> {
+
 }
