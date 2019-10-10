0214c4a1b552afa5cca9cd7e2af1118359108330
YaushevKamil <kyaush@mail.ru>
2019-10-11 02:35:01 +0300

    Scripted auto-commit on change (2019-10-11 02:35:01) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightWrightableComparable.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightWrightableComparable.java
@@ -1,4 +1,40 @@
 package ru.bmstu.hadoop.lab2;
 
-public class FlightWrightableComparable {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlightWrightableComparable implements WritableComparable {
+    private int counter;
+    private long timestamp;
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(counter);
+        out.writeLong(timestamp);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        counter = in.readInt();
+        timestamp = in.readLong();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        int thisValue = this.value;
+        int thatValue = o.value;
+        return (thisValue < thatValue ? -1 : (thisValue==thatValue ? 0 : 1));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + counter;
+        result = prime * result + (int) (timestamp ^ (timestamp >>> 32));
+        return result;
+    }
 }
