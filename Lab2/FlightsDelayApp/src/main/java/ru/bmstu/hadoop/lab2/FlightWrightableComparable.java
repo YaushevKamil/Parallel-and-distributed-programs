d640f00b5ac0ce43c0f9f16e9f7b603eee2efdd7
YaushevKamil <kyaush@mail.ru>
2019-10-11 14:41:17 +0300

    Scripted auto-commit on change (2019-10-11 14:41:17) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightWrightableComparable.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightWrightableComparable.java
@@ -55,6 +55,8 @@ public class FlightWrightableComparable implements WritableComparable {
             return Integer.compare(this.airportId, other.airportId);
         } else if (this.airTime != other.airTime) {
             return Float.compare(this.airTime, other.airTime);
+        } else if (this.airTime != other.airTime) {
+            return Float.compare(this.airTime, other.airTime);
         } else if (this.isCancelled ^ other.isCancelled) {
             return boolToInt(this.isCancelled) - boolToInt(other.isCancelled);
         } else if () {
