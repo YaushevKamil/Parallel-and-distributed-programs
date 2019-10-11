b8425529f4c7334cc741ebbdd2ae9e55cf41273c
YaushevKamil <kyaush@mail.ru>
2019-10-11 14:34:13 +0300

    Scripted auto-commit on change (2019-10-11 14:34:13) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightWrightableComparable.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightWrightableComparable.java
@@ -53,6 +53,8 @@ public class FlightWrightableComparable implements WritableComparable {
         FlightWrightableComparable other = (FlightWrightableComparable)o;
         if (this.isCancelled ^ other.isCancelled) {
             return boolToInt(this.isCancelled) - boolToInt(other.isCancelled);
+        } else if (this.isCancelled ^ other.isCancelled) {
+            return boolToInt(this.isCancelled) - boolToInt(other.isCancelled);
         } else if () {
 
         }
