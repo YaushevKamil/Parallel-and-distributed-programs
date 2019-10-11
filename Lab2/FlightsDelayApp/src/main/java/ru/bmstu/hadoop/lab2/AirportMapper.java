bc065d3b76bdb580958fa391fba799d63096ec2a
YaushevKamil <kyaush@mail.ru>
2019-10-11 18:07:16 +0300

    Scripted auto-commit on change (2019-10-11 18:07:15) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
@@ -1,4 +1,8 @@
 package ru.bmstu.hadoop.lab2;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
 public class AirportMapper {
 }
