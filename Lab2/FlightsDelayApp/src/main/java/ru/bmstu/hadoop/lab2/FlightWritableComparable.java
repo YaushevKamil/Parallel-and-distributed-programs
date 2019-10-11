c74afe23d0ef522c44f47318bc65edef9adc2294
YaushevKamil <kyaush@mail.ru>
2019-10-11 19:32:36 +0300

    Scripted auto-commit on change (2019-10-11 19:32:36) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightWritableComparable.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightWritableComparable.java
@@ -1,4 +1,67 @@
 package ru.bmstu.hadoop.lab2;
 
-public class FlightWritableComparable {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlightWritableComparable implements WritableComparable {
+    private int airportId;
+    private int dataType;
+
+    public FlightWritableComparable() {}
+
+    FlightWritableComparable(int airportId, int dataType) {
+        this.airportId = airportId;
+        this.dataType = dataType;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(airportId);
+        out.writeInt(dataType);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        airportId = in.readInt();
+        dataType = in.readInt();
+    }
+
+    private static int boolToInt(Boolean b) {
+        return Boolean.compare(b, false);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        FlightWritableComparable other = (FlightWritableComparable)o;
+        if (this.airportId != other.airportId) {
+            return Integer.compare(this.airportId, other.airportId);
+        } else if (this.dataType != other.dataType) {
+            return Float.compare(this.dataType, other.dataType);
+        } else return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        FlightWritableComparable other = (FlightWritableComparable)o;
+        return (this == o) || (this.airportId == other.airportId) && (this.dataType == other.dataType);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = airportId;
+        result = prime * result + dataType;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "airportId:" + airportId + "," +
+                "dataType:" + dataType +
+                "}";
+    }
 }
