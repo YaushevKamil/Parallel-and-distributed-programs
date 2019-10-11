bdf58116d02777b5a170d3f6a3d574b465739db9
YaushevKamil <kyaush@mail.ru>
2019-10-11 18:03:25 +0300

    Scripted auto-commit on change (2019-10-11 18:03:25) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
@@ -16,6 +16,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, FlightWrightableCom
         if (key.get() > 0) {
             String[] data = value.toString().split(",");
             float delayTime = !(data[DELAY_TIME].equals("")) ? Float.parseFloat(data[DELAY_TIME]) : 0.0f;
+            if ()
         }
     }
 }
