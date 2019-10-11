b9264f3bbc7ec1b93d3af3fbb0ca4bc5cd8c9009
YaushevKamil <kyaush@mail.ru>
2019-10-11 20:12:21 +0300

    Scripted auto-commit on change (2019-10-11 20:12:21) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsReducer.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsReducer.java
@@ -3,5 +3,6 @@ package ru.bmstu.hadoop.lab2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class FlightsReducer extends Reducer<FlightWritableComparable, Text> {
+public class FlightsReducer extends Reducer<FlightWritableComparable, Text, Text, Text> {
+    
 }
