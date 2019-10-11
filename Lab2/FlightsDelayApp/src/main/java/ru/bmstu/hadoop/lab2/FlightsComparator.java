27ff03fc6dc6128557b9c250494540576e2e45cc
YaushevKamil <kyaush@mail.ru>
2019-10-11 19:48:48 +0300

    Scripted auto-commit on change (2019-10-11 19:48:48) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsComparator.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsComparator.java
@@ -3,7 +3,7 @@ package ru.bmstu.hadoop.lab2;
 import org.apache.hadoop.io.WritableComparator;
 
 public class FlightsComparator extends WritableComparator {
-    
+    public FlightsComparator() {}
 
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
