ffa004fb902ae19d674d6fd90e41954a702276c2
YaushevKamil <kyaush@mail.ru>
2019-10-11 21:55:37 +0300

    Scripted auto-commit on change (2019-10-11 21:55:37) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
@@ -1,1 +1,36 @@
-package ru.bmstu.hadoop.lab2;import org.apache.hadoop.io.LongWritable;import org.apache.hadoop.io.Text;import org.apache.hadoop.mapreduce.Mapper;import java.io.IOException;public class AirportMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {    private static final int TYPE_AIRPORT = 0;    private static final int AIRPORT_ID = 0;    private static final int AIRPORT_NAME = 1;    private static final int INT_ZERO = 0;    private static int strToInt(String numString) {        int number = INT_ZERO;        try {            number = Integer.parseInt(numString);        } catch (Exception ignored) {}        return number;    }    @Override    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {        if (key.get() > 0) {            String[] data = value.toString().replaceAll("\"", "").split(",");            String airportId = data[AIRPORT_ID];            String airportName = data[AIRPORT_NAME];            context.write(new FlightWritableComparable(strToInt(airportId), TYPE_AIRPORT), new Text(airportName));        }    }}
+package ru.bmstu.hadoop.lab2;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class AirportMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
+    private static final int TYPE_AIRPORT = 0;
+    private static final int AIRPORT_ID = 0;
+    private static final int AIRPORT_NAME = 1;
+
+    private static final int INT_ZERO = 0;
+
+    private static int strToInt(String numString) {
+        int number = INT_ZERO;
+        try {
+            number = Integer.parseInt(numString);
+        } catch (Exception ignored) {}
+
+        return number;
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.get() > 0) {
+            String[] data = value.toString().replaceAll("\"", "").split(",");
+
+            String airportId = data[AIRPORT_ID];
+            String airportName = data[AIRPORT_NAME];
+
+            context.write(new FlightWritableComparable(strToInt(airportId), TYPE_AIRPORT), new Text(airportName));
+        }
+    }
+}
