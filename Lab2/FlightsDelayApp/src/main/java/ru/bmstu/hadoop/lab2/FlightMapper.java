30ab204bf7e4cda8fb2d22d2dd520ebe9c52bbd9
YaushevKamil <kyaush@mail.ru>
2019-10-11 18:41:50 +0300

    Scripted auto-commit on change (2019-10-11 18:41:50) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
@@ -1,44 +1,1 @@
-package ru.bmstu.hadoop.lab2;
-
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-
-import java.io.IOException;
-
-public class FlightMapper extends Mapper<LongWritable, Text, FlightWrightableComparable, Text> {
-    private static final int TYPE_FLIGHT = 1;
-    private static final int AIRPORT_ID = 14;
-    private static final int DELAY_TIME = 18;
-
-    private static int strToInt(String numString) {
-        int number = 0;
-        try {
-            number = Integer.parseInt(numString);
-        } catch (Exception ignored) {}
-
-        return number;
-    }
-
-    private static float strToFloat(String numString) {
-        float number = 0.0f;
-        try {
-            number = Float.parseFloat(numString);
-        } catch (Exception ignored) {}
-
-        return number;
-    }
-
-    @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        if (key.get() > 0) {
-            String[] data = value.toString().split(",");
-            
-            float delayTime = strToFloat(data[DELAY_TIME]);
-            if (delayTime > 0.0f) {
-                int airportId = strToInt(data[AIRPORT_ID]); /*Integer.parseInt(data[AIRPORT_ID]);*/
-                context.write(new FlightWrightableComparable(airportId, TYPE_FLIGHT), new Text(data[DELAY_TIME]));
-            }
-        }
-    }
-}
+package ru.bmstu.hadoop.lab2;import org.apache.hadoop.io.LongWritable;import org.apache.hadoop.io.Text;import org.apache.hadoop.mapreduce.Mapper;import java.io.IOException;public class FlightMapper extends Mapper<LongWritable, Text, FlightWrightableComparable, Text> {    private static final int TYPE_FLIGHT = 1;    private static final int AIRPORT_ID = 14;    private static final int DELAY_TIME = 18;    private static int strToInt(String numString) {        int number = 0;        try {            number = Integer.parseInt(numString);        } catch (Exception ignored) {}        return number;    }    private static float strToFloat(String numString) {        float number = 0.0f;        try {            number = Float.parseFloat(numString);        } catch (Exception ignored) {}        return number;    }    @Override    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {        if (key.get() > 0) {            String[] data = value.toString().split(",");                        float delayTime = strToFloat(data[DELAY_TIME]);            if (delayTime > 0.0f) {                int airportId = strToInt(data[AIRPORT_ID]); /*Integer.parseInt(data[AIRPORT_ID]);*/                context.write(new FlightWrightableComparable(airportId, TYPE_FLIGHT), new Text(data[DELAY_TIME]));            }        }    }}
