46d7ab039cb59f5b4ffafd4652a05636e3a834fa
YaushevKamil <kyaush@mail.ru>
2019-10-11 21:13:19 +0300

    Scripted auto-commit on change (2019-10-11 21:13:19) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsReducer.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsReducer.java
@@ -11,19 +11,29 @@ public class FlightsReducer extends Reducer<FlightWritableComparable, Text, Text
     private static final int INT_ZERO = 0;
     private static final float FLOAT_ZERO = 0.0f;
 
+    private static float strToFloat(String numString) {
+        float number = FLOAT_ZERO;
+        try {
+            number = Float.parseFloat(numString);
+        } catch (Exception ignored) {}
+
+        return number;
+    }
+
     @Override
     public void reduce(FlightWritableComparable key, Iterable<Text> values, Context context) throws IOException {
         Iterator<Text> iter = values.iterator();
         if (iter.hasNext()) {
             String airportName = "Airport: " + iter.next().toString();
             if (iter.hasNext()) {
-                int coount = INT_ZERO;
+                int count = INT_ZERO;
                 float min = FLOAT_ZERO;
                 float max = FLOAT_ZERO;
                 float sum = FLOAT_ZERO;
 
                 while (iter.hasNext()) {
-
+                    float curr = strToFloat(iter.next().toString());
+                    if (count == INT_ZERO || count ) {}
                 }
             }
         }
