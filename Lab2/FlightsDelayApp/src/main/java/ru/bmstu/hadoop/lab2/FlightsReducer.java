eef63bc9eca5815e217c367ff7ea12155c37176b
YaushevKamil <kyaush@mail.ru>
2019-10-11 20:13:48 +0300

    Scripted auto-commit on change (2019-10-11 20:13:48) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsReducer.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsReducer.java
@@ -8,6 +8,6 @@ import java.io.IOException;
 public class FlightsReducer extends Reducer<FlightWritableComparable, Text, Text, Text> {
 
     public void reduce(FlightWritableComparable key, Iterable<Text> values, Context context) throws IOException {
-
+        
     }
 }
