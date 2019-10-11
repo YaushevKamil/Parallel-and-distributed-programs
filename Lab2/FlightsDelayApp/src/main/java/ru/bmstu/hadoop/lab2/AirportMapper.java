3811becbec0a8cebea874fc5ab626477aa99dc38
YaushevKamil <kyaush@mail.ru>
2019-10-11 18:07:39 +0300

    Scripted auto-commit on change (2019-10-11 18:07:39) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
@@ -4,5 +4,21 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportMapper {
+import java.io.IOException;
+
+public class AirportMapper extends Mapper<LongWritable, Text, FlightWrightableComparable, Text> {
+    private static final int AIRPORT_ID = 14;
+    private static final int DELAY_TIME = 18;
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.get() > 0) {
+            String[] data = value.toString().split(",");
+            float delayTime = !(data[DELAY_TIME].equals("")) ? Float.parseFloat(data[DELAY_TIME]) : 0.0f;
+            if (delayTime > 0.0f) {
+                int airportId = Integer.parseInt(data[AIRPORT_ID]);
+                context.write(new FlightWrightableComparable(airportId, 1), new Text(data[DELAY_TIME]));
+            }
+        }
+    }
 }
