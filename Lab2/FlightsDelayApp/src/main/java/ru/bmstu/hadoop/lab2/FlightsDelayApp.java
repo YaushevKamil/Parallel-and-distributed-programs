d7ab86a1e813b0a07766b67867b6e0a524915014
YaushevKamil <kyaush@mail.ru>
2019-10-11 19:19:34 +0300

    Scripted auto-commit on change (2019-10-11 19:19:34) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsDelayApp.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsDelayApp.java
@@ -18,7 +18,7 @@ public class FlightsDelayApp {
         Job job = Job.getInstance();
         job.setJarByClass(FlightsDelayApp.class);
         job.setJobName("Flights Delay");
-        MultipleInputs.addInputPath(job, new Path(args[0]));
+        MultipleInputs.addInputPath(job, new Path(args[0]), );
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
