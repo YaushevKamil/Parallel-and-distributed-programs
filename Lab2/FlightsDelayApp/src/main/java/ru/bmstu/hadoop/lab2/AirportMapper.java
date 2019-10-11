0214e4766a959bb16ba73011f897ac99d4c48c25
YaushevKamil <kyaush@mail.ru>
2019-10-11 21:58:45 +0300

    Scripted auto-commit on change (2019-10-11 21:58:44) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
@@ -21,7 +21,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, FlightWritableComp
 
         return number;
     }
-    
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() > 0) {
