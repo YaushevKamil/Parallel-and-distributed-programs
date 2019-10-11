89bbb246f5f4544680d07d90b784d44ad8ad849b
YaushevKamil <kyaush@mail.ru>
2019-10-11 19:36:17 +0300

    Scripted auto-commit on change (2019-10-11 19:36:17) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsPartitioner.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightsPartitioner.java
@@ -4,7 +4,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class FlightsPartitioner extends Partitioner<FlightWritableComparable, Text> {
+    @Override
     public int getPartition(JoinWritableComparable key, Text value, int numReduceTasks) {
-
+        
     }
 }
