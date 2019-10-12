380f909858a4dad5089dd4ac40a10e8347506629
YaushevKamil <kyaush@mail.ru>
2019-10-12 23:11:07 +0300

    Scripted auto-commit on change (2019-10-12 23:11:07) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/CSVUtils.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/CSVUtils.java
@@ -1,16 +1,16 @@
 package ru.bmstu.hadoop.lab2;
 
 class CSVUtils {
-    static final int INT_ZERO = 0;
+    static final int   INT_ZERO   = 0;
     static final float FLOAT_ZERO = 0.0f;
 
     static final int TYPE_AIRPORT = 0;
-    static final int TYPE_FLIGHT = 1;
+    static final int TYPE_FLIGHT  = 1;
 
     private static final int ORIGIN_AIRPORT_ID_COLUMN = 14;
-    private static final int DELAY_TIME_COLUMN = 18;
+    private static final int DELAY_TIME_COLUMN        = 18;
 
-    private static final int AIRPORT_ID_COLUMN = 0;
+    private static final int AIRPORT_ID_COLUMN   = 0;
     private static final int AIRPORT_NAME_COLUMN = 1;
 
      private static String[] airportData;
