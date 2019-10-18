798201b28b827a3ac54b7eb70cde199ca7876a86
YaushevKamil <kyaush@mail.ru>
2019-10-18 22:19:00 +0300

    Scripted auto-commit on change (2019-10-18 22:19:00) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
@@ -58,7 +58,7 @@ public class FlightsDelayApp {
                                 airportsBroadcast.value().get(k._1()._2()) + "\t"   +
                                 k._2() +
                            "}");
-        
+
         result.saveAsTextFile(OUTPUT_FILE);
     }
 }
