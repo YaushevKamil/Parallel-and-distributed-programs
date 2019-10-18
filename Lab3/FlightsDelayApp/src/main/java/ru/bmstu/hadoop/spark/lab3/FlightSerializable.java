332617ed4c8afd6c9b83deee8708f9c6f6a4716d
YaushevKamil <kyaush@mail.ru>
2019-10-18 18:37:05 +0300

    Scripted auto-commit on change (2019-10-18 18:37:05) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightSerializable.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightSerializable.java
@@ -3,7 +3,7 @@ package ru.bmstu.hadoop.spark.lab3;
 import java.io.Serializable;
 
 public class FlightSerializable implements Serializable {
-    
+    private int 
 
     public FlightSerializable(int originAirportID, int destAirportID, float delayTime, boolean isCancelled) {
     }
