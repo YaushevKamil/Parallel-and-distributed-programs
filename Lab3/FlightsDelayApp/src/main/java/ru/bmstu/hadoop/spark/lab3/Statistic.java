605dac6106dc221b7c642933f17b0489a7513227
YaushevKamil <kyaush@mail.ru>
2019-10-18 19:14:41 +0300

    Scripted auto-commit on change (2019-10-18 19:14:41) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/Statistic.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/Statistic.java
@@ -16,4 +16,36 @@ public class Statistic implements Serializable {
         this.cancelledFlightsCount = cancelledFlightsCount;
         this.maxDelay = maxDelay;
     }
+
+    public int getFlightsCount() {
+        return flightsCount;
+    }
+
+    public int getDelayedFlightsCount() {
+        return delayedFlightsCount;
+    }
+
+    public int getCancelledFlightsCount() {
+        return cancelledFlightsCount;
+    }
+
+    public float getMaxDelay() {
+        return maxDelay;
+    }
+
+    public void setFlightsCount(int flightsCount) {
+        this.flightsCount = flightsCount;
+    }
+
+    public void setDelayedFlightsCount(int delayedFlightsCount) {
+        this.delayedFlightsCount = delayedFlightsCount;
+    }
+
+    public void setCancelledFlightsCount(int cancelledFlightsCount) {
+        this.cancelledFlightsCount = cancelledFlightsCount;
+    }
+
+    public void setMaxDelay(float maxDelay) {
+        this.maxDelay = maxDelay;
+    }
 }
