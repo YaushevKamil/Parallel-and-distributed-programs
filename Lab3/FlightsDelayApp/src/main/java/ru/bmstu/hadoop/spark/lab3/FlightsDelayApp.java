c88a70241c27240c961f1451c738f447292d7a42
YaushevKamil <kyaush@mail.ru>
2019-10-18 18:56:54 +0300

    Scripted auto-commit on change (2019-10-18 18:56:54) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
@@ -30,7 +30,9 @@ public class FlightsDelayApp {
     JavaPairRDD<Integer, String> airportsData = airportsTable
             .filter(s -> !s.contains(AIRPORTS_FIRST_COLUMN))
             .mapToPair(s -> {
-                return new Tuple2<>()
+                return new Tuple2<>(
+                        
+                )
             });
 
     final Broadcast<Map<String, AirportData>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
