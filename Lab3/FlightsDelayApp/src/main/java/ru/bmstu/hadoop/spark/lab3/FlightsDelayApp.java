f04889ca2f1b7b32609971386d06456ecd2612db
YaushevKamil <kyaush@mail.ru>
2019-10-18 20:11:29 +0300

    Scripted auto-commit on change (2019-10-18 20:11:29) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
@@ -54,5 +54,5 @@ public class FlightsDelayApp {
                     Statistic::add).mapToPair(map -> new Tuple2<>(map._1(), Statistic.resString(map._2()))
             );
 
-    JavaRDD<String> result = getap
+    JavaRDD<String> result = flightDataStat.
 }
