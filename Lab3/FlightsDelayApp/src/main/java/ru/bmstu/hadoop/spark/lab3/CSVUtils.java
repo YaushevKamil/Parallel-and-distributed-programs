8927ea184f9d3f3bc37a3a4a0c17c287612cabe8
YaushevKamil <kyaush@mail.ru>
2019-10-18 18:29:18 +0300

    Scripted auto-commit on change (2019-10-18 18:29:18) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/CSVUtils.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/CSVUtils.java
@@ -10,7 +10,7 @@ class CSVUtils {
     private static final int ORIGIN_AIRPORT_ID_COLUMN = 11;
     private static final int DEST_AIRPORT_ID_COLUMN   = 14;
     private static final int DELAY_TIME_COLUMN        = 18;
-    private static final int IS_CANCELLED             = 19;
+    private static final int IS_CANCELLED_COLUMN      = 19;
 
     private static final int AIRPORT_ID_COLUMN   = 0;
     private static final int AIRPORT_NAME_COLUMN = 1;
@@ -69,6 +69,6 @@ class CSVUtils {
 
 
     public static boolean getCancelled() {
-        
+        return strToInt(flightData[IS_CANCELLED_COLUMN]) != 0;
     }
 }
