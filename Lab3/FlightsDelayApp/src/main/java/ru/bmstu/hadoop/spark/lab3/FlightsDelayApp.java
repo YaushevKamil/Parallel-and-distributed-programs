45936cfe0e255b5495551e236c469a2ef4cb955e
YaushevKamil <kyaush@mail.ru>
2019-10-18 17:55:37 +0300

    Scripted auto-commit on change (2019-10-18 17:55:37) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
@@ -14,5 +14,7 @@ public class FlightsDelayApp {
 
     JavaPairRDD<Integer, String> flightData = flightsTable
             .filter(s -> !s.contains("YEAR"))
-            .mapToPair()
+            .mapToPair(s -> {
+                
+            })
 }
