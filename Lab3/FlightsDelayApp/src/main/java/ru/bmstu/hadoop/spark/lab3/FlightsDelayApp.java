3d12b7a22c24a614fc3718378d856cdb55ed569e
YaushevKamil <kyaush@mail.ru>
2019-10-18 19:08:32 +0300

    Scripted auto-commit on change (2019-10-18 19:08:32) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
@@ -41,7 +41,7 @@ public class FlightsDelayApp {
                         new FlightSerializable(originAirportID, destAirportID, delayTime, isCancelled));
             });
 
-    JavaPairRDD<Tuple2<Integer, Integer>, String>
+    JavaPairRDD<Tuple2<Integer, Integer>, String> flightData
 
     JavaRDD<String> result = getap
 }
