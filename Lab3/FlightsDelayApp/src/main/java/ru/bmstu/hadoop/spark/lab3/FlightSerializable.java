0fe82fa04fb8c883c84aaa374c7141770773f889
YaushevKamil <kyaush@mail.ru>
2019-10-18 18:47:20 +0300

    Scripted auto-commit on change (2019-10-18 18:47:20) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightSerializable.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightSerializable.java
@@ -51,6 +51,7 @@ public class FlightSerializable implements Serializable {
 
     @Override
     public String toString() {
-        return "FlightSerializable"
+        return "FlightSerializable" + "{"
+                ""
     }
 }
