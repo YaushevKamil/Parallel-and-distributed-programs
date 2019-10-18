9cc992885241cdbc1bc910e81f779b6a9fda6482
YaushevKamil <kyaush@mail.ru>
2019-10-18 17:39:12 +0300

    Scripted auto-commit on change (2019-10-18 17:39:12) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
@@ -1,6 +1,7 @@
 package ru.bmstu.hadoop.spark.lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
@@ -11,5 +12,5 @@ public class FlightsDelayApp {
     JavaRDD<String> flightsTable = sc.textFile("664600583_T_ONTIME_sample.csv");
     JavaRDD<String> airportsTable = sc.textFile("L_AIRPORT_ID.csv");
 
-    
+    JavaPairRDD<Integer, String> 
 }
