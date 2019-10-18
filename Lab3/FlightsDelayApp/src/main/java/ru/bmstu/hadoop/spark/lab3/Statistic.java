c6683109aa2a15fa487e09592f3cdd203f0f2df6
YaushevKamil <kyaush@mail.ru>
2019-10-18 19:11:17 +0300

    Scripted auto-commit on change (2019-10-18 19:11:17) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/Statistic.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/Statistic.java
@@ -3,5 +3,7 @@ package ru.bmstu.hadoop.spark.lab3;
 import java.io.Serializable;
 
 public class Statistic implements Serializable {
+    private int flightsCount;
+    private int delayedFlightsCount;
     
 }
