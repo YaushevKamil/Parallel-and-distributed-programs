2810497ded94b3b56fa777bfe885406f682d94d5
YaushevKamil <kyaush@mail.ru>
2019-10-18 20:23:23 +0300

    Scripted auto-commit on change (2019-10-18 20:23:23) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/CSVUtils.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/CSVUtils.java
@@ -19,9 +19,9 @@ class CSVUtils {
     private static final int AIRPORT_ID_COLUMN   = 0;
     private static final int AIRPORT_NAME_COLUMN = 1;
 
-    static final String FLIGHTS_FIRST_COLUMN = "YEAR";
+    static final String FLIGHTS_FIRST_COLUMN  = "YEAR";
     static final String AIRPORTS_FIRST_COLUMN = "Code";
-    
+
     private static String[] airportData;
     private static String[] flightData;
 
