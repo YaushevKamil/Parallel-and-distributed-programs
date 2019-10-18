7cae9c25c63a0f15287328e410f579e47a5ae829
YaushevKamil <kyaush@mail.ru>
2019-10-18 19:57:31 +0300

    Scripted auto-commit on change (2019-10-18 19:57:31) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/Statistic.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/Statistic.java
@@ -64,7 +64,7 @@ public class Statistic implements Serializable {
     }
 
     static String resString(Statistic stat) {
-        float delayPercent = stat.setFlightsCount() / stat.;
+        float delayPercent = stat.setDelayedFlightsCount() / stat.getFlightsCount() * 100.f;
     }
 
     @Override
