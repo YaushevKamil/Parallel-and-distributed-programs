1f66b36608900465e9f26779a9a0f63901377305
YaushevKamil <kyaush@mail.ru>
2019-10-18 17:33:27 +0300

    Scripted auto-commit on change (2019-10-18 17:33:27) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsDelayApp.java
@@ -1,8 +1,9 @@
 package ru.bmstu.hadoop.spark.lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class FlightsDelayApp {
     SparkConf conf = new SparkConf().setAppName("lab5");
-    
+    JavaSparkContext sc = new JavaSparkContext(conf);
 }
