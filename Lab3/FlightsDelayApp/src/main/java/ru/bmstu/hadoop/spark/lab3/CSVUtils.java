852c7ca029ed4cc07dea030bb3ce274d2e747e3b
YaushevKamil <kyaush@mail.ru>
2019-10-18 22:10:35 +0300

    Scripted auto-commit on change (2019-10-18 22:10:35) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/CSVUtils.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/CSVUtils.java
@@ -11,9 +11,6 @@ class CSVUtils {
     static final int  INT_ZERO   = 0;
     static final float FLOAT_ZERO = 0.0f;
 
-    static final int TYPE_AIRPORT = 0;
-    static final int TYPE_FLIGHT  = 1;
-
     private static final int ORIGIN_AIRPORT_ID_COLUMN = 11;
     private static final int DEST_AIRPORT_ID_COLUMN   = 14;
     private static final int DELAY_TIME_COLUMN        = 18;
@@ -25,13 +22,13 @@ class CSVUtils {
     private static String[] airportData;
     private static String[] flightData;
 
-    static int strToInt(String numString) {
+    private static int strToInt(String numString) {
         return numString.length() > 0 ?
                 Integer.parseInt(numString) :
                 INT_ZERO;
     }
 
-    static float strToFloat(String numString) {
+    private static float strToFloat(String numString) {
         return numString.length() > 0 ?
                 Float.parseFloat(numString):
                 FLOAT_ZERO;
