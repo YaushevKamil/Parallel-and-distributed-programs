526c7627aa1a5ee56371ead22a3249de6d6eb5a8
YaushevKamil <kyaush@mail.ru>
2019-10-18 19:37:21 +0300

    Scripted auto-commit on change (2019-10-18 19:37:21) by gitwatch.sh

--- Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/Statistic.java
+++ Lab3/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/spark/lab3/Statistic.java
@@ -49,7 +49,7 @@ public class Statistic implements Serializable {
         this.maxDelay = maxDelay;
     }
 
-    static void addValue(Statistic stat, boolean hasDelayed) {
+    static void addValue(Statistic stat, boolean isDelayed, boolean isCancelled, ) {
 
     }
 
