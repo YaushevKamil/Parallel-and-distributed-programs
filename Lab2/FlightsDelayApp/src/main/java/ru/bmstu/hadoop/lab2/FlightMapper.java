2fdc23692c387ebe7e5b9a66ce63fa9b268da76b
YaushevKamil <kyaush@mail.ru>
2019-10-24 16:36:58 +0300

    Scripted auto-commit on change (2019-10-24 16:36:58) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
@@ -11,15 +11,14 @@ import static ru.bmstu.hadoop.lab2.CSVUtils.*;
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        if (key.get() == 0) {
-            return;
-        }
-        CSVUtils csvData = new CSVUtils(value.toString());
-        int airportId = csvData.getOriginAirportId();
-        float floatDelayTime = csvData.getFloatDelayTime();
-        String stringDelayTime = csvData.getStringDelayTime();
-        if (floatDelayTime > FLOAT_ZERO) {
-            context.write(new FlightWritableComparable(airportId, TYPE_FLIGHT), new Text(stringDelayTime));
+        if (key.get() > 0){
+            CSVUtils csvData = new CSVUtils(value.toString());
+            int airportId = csvData.getOriginAirportId();
+            float floatDelayTime = csvData.getFloatDelayTime();
+            String stringDelayTime = csvData.getStringDelayTime();
+            if (floatDelayTime > FLOAT_ZERO) {
+                context.write(new FlightWritableComparable(airportId, TYPE_FLIGHT), new Text(stringDelayTime));
+            }
         }
     }
 }
