c80f61824ec2c1c33fe4fe2d86727dfa7a7fc297
YaushevKamil <kyaush@mail.ru>
2019-10-24 16:37:23 +0300

    Scripted auto-commit on change (2019-10-24 16:37:23) by gitwatch.sh

--- Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
+++ Lab2/FlightsDelayApp/src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
@@ -11,12 +11,11 @@ import static ru.bmstu.hadoop.lab2.CSVUtils.*;
 public class AirportMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        if (key.get() == 0) {
-            return;
+        if (key.get() > 0) {
+            CSVUtils csvData = new CSVUtils(value.toString());
+            int airportId = csvData.getAirportId();
+            String airportName = csvData.getAirportName();
+            context.write(new FlightWritableComparable(airportId, TYPE_AIRPORT), new Text(airportName));
         }
-        CSVUtils csvData = new CSVUtils(value.toString());
-        int airportId = csvData.getAirportId();
-        String airportName = csvData.getAirportName();
-        context.write(new FlightWritableComparable(airportId, TYPE_AIRPORT), new Text(airportName));
     }
 }
