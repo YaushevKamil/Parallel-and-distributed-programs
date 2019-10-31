824d5f42bba470b86b6ba31acb099ab97b5de755
YaushevKamil <kyaush@mail.ru>
2019-10-31 19:10:31 +0300

    Scripted auto-commit on change (2019-10-31 19:10:31) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/Test.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/Test.java
@@ -17,4 +17,6 @@ public class Test {
     @JsonProperty("testName") private String testName;
     @JsonProperty("expectedResult") private String expectedResult;
     @JsonProperty("params") private List<String> params;
+
+    
 }
