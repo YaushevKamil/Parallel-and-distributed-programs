acdf56fef20c19735ce00caf12c222ae84a56c4f
YaushevKamil <kyaush@mail.ru>
2019-10-31 19:24:42 +0300

    Scripted auto-commit on change (2019-10-31 19:24:42) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/Tests.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/Tests.java
@@ -1,7 +1,6 @@
 package ru.bmstu.akka.lab4;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.*;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
