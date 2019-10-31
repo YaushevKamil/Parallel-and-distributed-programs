06d9250a432654fdac1757c737eb57acb1229308
YaushevKamil <kyaush@mail.ru>
2019-10-31 16:56:34 +0300

    Scripted auto-commit on change (2019-10-31 16:56:34) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/PerformActor.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/PerformActor.java
@@ -1,10 +1,11 @@
 package ru.bmstu.akka.lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class PerformActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return Recieve;
+        return ReceiveBuilder;
     }
 }
