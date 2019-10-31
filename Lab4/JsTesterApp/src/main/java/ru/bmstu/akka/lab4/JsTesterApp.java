58b01499478023c7c2086441f3883ee3eea1615b
YaushevKamil <kyaush@mail.ru>
2019-10-31 14:03:34 +0300

    Scripted auto-commit on change (2019-10-31 14:03:34) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
@@ -1,6 +1,10 @@
 package ru.bmstu.akka.lab4;
 
+import akka.actor.Actor;
+
 public class JsTesterApp {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Actor
+    }
 
 }
