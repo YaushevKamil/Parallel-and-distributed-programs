58f1436f15d63638d284b78234e66309605338a1
YaushevKamil <kyaush@mail.ru>
2019-10-31 20:13:04 +0300

    Scripted auto-commit on change (2019-10-31 20:13:04) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
@@ -63,6 +63,8 @@ public class JsTesterApp extends AllDirectives {
                         ConnectHttp.toHost("localhost", 8080),
                         materializer
                 );
+        System.in.read();
+
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
 
     }
