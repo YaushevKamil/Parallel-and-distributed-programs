a9ba0dc06e4dbf9fccdb5cf71e1a1c05076890ad
YaushevKamil <kyaush@mail.ru>
2019-10-31 17:54:59 +0300

    Scripted auto-commit on change (2019-10-31 17:54:59) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
@@ -12,6 +12,9 @@ public class JsTesterApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("lab4");
         storeActor = system.actorOf(Props.create(StoreActor.class));
-        routeActor = system.actorOf(new RoundRobinPool(5).withSupervisorStrategy(strategy).props(Props.create(), "router"));
+        routeActor = system.actorOf(
+                new RoundRobinPool(5)
+                        .withSupervisorStrategy(strategy)
+                        .props(Props.create(), "router"));
     }
 }
