b6921c7e12efb3a5520c5a7af810e123413856da
YaushevKamil <kyaush@mail.ru>
2019-10-31 15:01:15 +0300

    Scripted auto-commit on change (2019-10-31 15:01:15) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/ResponseMessage.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/ResponseMessage.java
@@ -3,11 +3,10 @@ package ru.bmstu.akka.lab4;
 import java.util.ArrayList;
 
 public class ResponseMessage {
-    private String packageId;
     private ArrayList<String> results;
 
     public ResponseMessage(String packageId, ArrayList<String> results) {
-        this.packageId = packageId;
+        super(packageId);
         this.results = results;
     }
 
