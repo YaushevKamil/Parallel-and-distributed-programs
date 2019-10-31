4aecb60da21544a611f010cefd1a5837365d51ef
YaushevKamil <kyaush@mail.ru>
2019-10-31 17:09:56 +0300

    Scripted auto-commit on change (2019-10-31 17:09:56) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/PerformActor.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/PerformActor.java
@@ -4,13 +4,14 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 
 public class PerformActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(JsFunction.class, m -> {
-                    ScriptEngine engine = new
+                    ScriptEngine engine = new ScriptEngineManager().getEngineByName();
                 })
                 .build();
     }
