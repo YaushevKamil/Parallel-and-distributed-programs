8c4e403fdf6934ae9abd7ad117a36174405485ca
YaushevKamil <kyaush@mail.ru>
2019-10-31 14:03:54 +0300

    Scripted auto-commit on change (2019-10-31 14:03:54) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
@@ -5,7 +5,7 @@ import akka.actor.ActorSystem;
 
 public class JsTesterApp {
     public static void main(String[] args) {
-        ActorSystem system = ActorSystem.create();
+        ActorSystem system = ActorSystem.create("");
     }
 
 }
