8f78e77787a8d402e96c8e58b6f5b568103e4903
YaushevKamil <kyaush@mail.ru>
2019-10-31 15:02:42 +0300

    Scripted auto-commit on change (2019-10-31 15:02:42) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreMessage.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreMessage.java
@@ -1,16 +1,11 @@
 package ru.bmstu.akka.lab4;
 
 public class StoreMessage extends Message {
-    private String packageId;
     private String result;
 
     public StoreMessage(String packageId, String result) {
         super(packageId);
         this.result = result;
-    }
-
-    public String getPackageId() {
-        return packageId;
     }
 
     public String getResult() {
