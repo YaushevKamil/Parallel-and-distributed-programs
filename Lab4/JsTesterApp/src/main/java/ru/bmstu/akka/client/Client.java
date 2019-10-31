124df676aa240aeea92d9443b0ac329ca1eb03cb
YaushevKamil <kyaush@mail.ru>
2019-10-31 22:04:23 +0300

    Scripted auto-commit on change (2019-10-31 22:04:23) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/client/Client.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/client/Client.java
@@ -1,10 +1,12 @@
 package ru.bmstu.akka.client;
 
 import akka.actor.ActorSystem;
+import akka.stream.Materializer;
 
 public class Client {
     public static void main(String[] args) {
         final ActorSystem system = ActorSystem.create();
+        final Materializer materializer = ActorMaterializer.create(system);
 
     }
 }
