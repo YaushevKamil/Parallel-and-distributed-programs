a7e9c746ff209d7c18f58d2c5023a07ba469596c
YaushevKamil <kyaush@mail.ru>
2019-10-31 18:15:22 +0300

    Scripted auto-commit on change (2019-10-31 18:15:22) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
@@ -49,7 +49,21 @@ public class JsTesterApp extends AllDirectives {
 
     private Route createRoute(ActorSystem system) {
         return route(
-                
+                path("test", () ->
+                        route(
+                                post(() ->
+                                        entity(Jackson.unmarshaller(TestPackageMsg.class), msg -> {
+                                            testPackageActor.tell(msg, ActorRef.noSender());
+                                            return complete("Test started!");
+                                        })))),
+                path("put", () ->
+                        get(() ->
+                                parameter("key", (key) ->
+                                        parameter("value", (value) ->
+                                        {
+                                            storeActor.tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
+                                            return complete("value saved to store ! key=" + key + " value=" + value);
+                                        })))
         );
     }
 }
