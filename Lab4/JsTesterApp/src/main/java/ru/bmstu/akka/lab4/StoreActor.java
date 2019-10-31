732d170f8742a15e5f604936a90452db32559f5e
YaushevKamil <kyaush@mail.ru>
2019-10-31 14:40:32 +0300

    Scripted auto-commit on change (2019-10-31 14:40:32) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreActor.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreActor.java
@@ -25,7 +25,7 @@ public class StoreActor extends AbstractActor {
                     }
                 })
                 .match(GetMessage.class, req -> {
-                    
+
                 });
     }
 }
