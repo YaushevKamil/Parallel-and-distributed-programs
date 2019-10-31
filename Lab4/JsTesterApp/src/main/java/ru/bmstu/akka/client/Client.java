7b28414b2edeb2a2f585fc6cfcd4f5f1657d8344
YaushevKamil <kyaush@mail.ru>
2019-10-31 22:10:31 +0300

    Scripted auto-commit on change (2019-10-31 22:10:31) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/client/Client.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/client/Client.java
@@ -18,6 +18,7 @@ public class Client {
         String json = "";
         json = new Scanner(new File("tests.json")).useDelimiter("\\Z").next();
 
-        final CompletionStage<HttpResponse> postFuture = 
+        final CompletionStage<HttpResponse> postFuture =
+                
     }
 }
