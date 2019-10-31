4b1c5ae8509ad34b5cf3fc990ef0be66f379d90a
YaushevKamil <kyaush@mail.ru>
2019-10-31 22:04:11 +0300

    Scripted auto-commit on change (2019-10-31 22:04:11) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/client/Client.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/client/Client.java
@@ -1,7 +1,10 @@
 package ru.bmstu.akka.client;
 
+import akka.actor.ActorSystem;
+
 public class Client {
     public static void main(String[] args) {
-        
+        final ActorSystem system = ActorSystem.create();
+
     }
 }
