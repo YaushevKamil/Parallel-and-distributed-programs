2342fd5051c90155382d2dbe6a3d6ba79d32e19b
YaushevKamil <kyaush@mail.ru>
2019-10-31 20:11:12 +0300

    Scripted auto-commit on change (2019-10-31 20:11:12) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
@@ -57,7 +57,7 @@ public class JsTesterApp extends AllDirectives {
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 tester.createRoute(system).flow(system, materializer);
 
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp)
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost(()))
 
     }
 
