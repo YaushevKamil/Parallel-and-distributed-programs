d177c27ffca6aa303916dd7cf98e1970f9a11c6d
YaushevKamil <kyaush@mail.ru>
2019-10-31 14:36:09 +0300

    Scripted auto-commit on change (2019-10-31 14:36:09) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreActor.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreActor.java
@@ -19,7 +19,7 @@ public class StoreActor extends AbstractActor {
                     if (store.containsKey(packageId)) {
                         store.get(packageId).add(result);
                     } else {
-                        
+                        ArrayList<String> 
                     }
                     store.put(m.getKey(), m.getValue());
 
