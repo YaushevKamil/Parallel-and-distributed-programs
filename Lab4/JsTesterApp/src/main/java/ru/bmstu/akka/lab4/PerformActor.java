829f9157823bd95782a3f63e0892a95ac6f32090
YaushevKamil <kyaush@mail.ru>
2019-10-31 17:14:33 +0300

    Scripted auto-commit on change (2019-10-31 17:14:32) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/PerformActor.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/PerformActor.java
@@ -15,6 +15,7 @@ public class PerformActor extends AbstractActor {
                     ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
                     engine.eval(m.getScript());
                     Invocable invocable = (Invocable) engine;
+                    String result = 
                     return invocable.invokeFunction()
                 })
                 .build();
