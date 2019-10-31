ed626e2e43c5137733a7121d181d0a8d7c76b338
YaushevKamil <kyaush@mail.ru>
2019-10-31 17:56:31 +0300

    Scripted auto-commit on change (2019-10-31 17:56:31) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
@@ -17,6 +17,6 @@ public class JsTesterApp {
                 .withSupervisorStrategy(strategy)
                 .props(Props.create(), "router"));
         final Http http = Http.get(system);
-        final 
+        final Actor
     }
 }
