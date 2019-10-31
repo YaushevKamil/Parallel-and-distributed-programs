552db2a47d7abd439328ab86fc947fcaf532e0b8
YaushevKamil <kyaush@mail.ru>
2019-10-31 14:58:07 +0300

    Scripted auto-commit on change (2019-10-31 14:58:07) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/Message.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/Message.java
@@ -1,4 +1,11 @@
 package ru.bmstu.akka.lab4;
 
 public class Message {
+    private String packageId;
+    private String result;
+
+    public Message(String packageId, String result) {
+        this.packageId = packageId;
+        this.result = result;
+    }
 }
