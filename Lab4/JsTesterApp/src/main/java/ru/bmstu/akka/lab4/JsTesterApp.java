435bf6ab23f68fb8f83670881374cf9c67c88f32
YaushevKamil <kyaush@mail.ru>
2019-10-31 19:59:20 +0300

    Scripted auto-commit on change (2019-10-31 19:59:20) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
@@ -73,7 +73,7 @@ public class JsTesterApp extends AllDirectives {
                 path("put", () ->
                         get(() -> parameter("packageId", (packageId) -> {
                             System.out.println("get()");
-                                            storeActor.tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
+                            .tell(new StoreActor.StoreMessage(key, value), ActorRef.noSender());
                                             return complete("value saved to store ! key=" + key + " value=" + value);
                                         })))
         );
