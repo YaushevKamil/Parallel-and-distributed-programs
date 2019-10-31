4547197925d5a355f2806e62bb524db9341c7273
YaushevKamil <kyaush@mail.ru>
2019-10-31 22:13:05 +0300

    Scripted auto-commit on change (2019-10-31 22:13:05) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/client/Client.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/client/Client.java
@@ -22,6 +22,6 @@ public class Client {
 
         final CompletionStage<HttpResponse> postFuture =
                 Http.get(system)
-                .singleRequest(HttpRequest.POST("http://localhost:8080/tests").withEntity(MediaTypes.APPLICATION_JSON.toContentType(), json), )
+                .singleRequest(HttpRequest.POST("http://localhost:8080/tests").withEntity(MediaTypes.APPLICATION_JSON.toContentType(), json), materializer)
     }
 }
