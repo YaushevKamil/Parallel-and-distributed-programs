1625c15d4e3c37705fe8b5a9f8e049bc0c3fcab8
YaushevKamil <kyaush@mail.ru>
2019-10-31 17:03:41 +0300

    Scripted auto-commit on change (2019-10-31 17:03:41) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsFunction.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsFunction.java
@@ -1,11 +1,13 @@
 package ru.bmstu.akka.lab4;
 
 import java.sql.Struct;
+import java.util.List;
 
 public class JsFunction {
     private String packageId;
     private String functionName;
     private String script;
+    private List<>
     private String expectedResult;
 
 
