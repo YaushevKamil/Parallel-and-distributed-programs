aaa8110879f74df77560748cc19ad9252af6152e
YaushevKamil <kyaush@mail.ru>
2019-10-31 17:30:11 +0300

    Scripted auto-commit on change (2019-10-31 17:30:11) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/PerformActor.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/PerformActor.java
@@ -28,7 +28,7 @@ public class PerformActor extends AbstractActor {
                     try {
                         String result = PerformActor.performScript(m.getFunctionName(), m.getScript(), m.getParams());
                     } catch (ScriptException e) {
-                        description = "Error: ScriptException";
+                        description = "Error: ScriptException\n" + e.get;
                     } catch (NoSuchMethodException e) {
 
                     }
