55ee8838293365f704fe8f6431785e2a9b29e0a7
YaushevKamil <kyaush@mail.ru>
2019-10-31 14:16:26 +0300

    Scripted auto-commit on change (2019-10-31 14:16:26) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreActor.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreActor.java
@@ -5,7 +5,7 @@ import akka.actor.AbstractActor;
 import java.util.ArrayList;
 
 public class StoreActor extends AbstractActor {
-    private Map<String, ArrayList<Test>> 
+    private Map<String, ArrayList<Test>> storage = 
     @Override
     public Receive createReceive() {
         return null;
