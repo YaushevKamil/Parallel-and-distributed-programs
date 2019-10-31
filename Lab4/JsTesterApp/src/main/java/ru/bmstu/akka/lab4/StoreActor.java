df9c01a73b8889d1e53930ddfc9ed6a27c486c23
YaushevKamil <kyaush@mail.ru>
2019-10-31 14:49:09 +0300

    Scripted auto-commit on change (2019-10-31 14:49:09) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreActor.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/StoreActor.java
@@ -25,7 +25,8 @@ public class StoreActor extends AbstractActor {
                     }
                 })
                 .match(GetMessage.class, req -> {
-                    sender().tell(new ResponseMessage(req.getPackageId(), store.get(req.getPackageId())), self());
+                    String packageId = req.getPackageId();
+                    sender().tell(new ResponseMessage(req.getPackageId(), store.get(req.getPackageId())), getSelf());
                 })
                 .build();
     }
