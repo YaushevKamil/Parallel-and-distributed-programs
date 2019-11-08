2701dcedf398cdbc5b8fa39aefb97770206c44f1
YaushevKamil <kyaush@mail.ru>
2019-11-08 21:02:36 +0300

    Scripted auto-commit on change (2019-11-08 21:02:36) by gitwatch.sh

--- Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
+++ Lab4/JsTesterApp/src/main/java/ru/bmstu/akka/lab4/JsTesterApp.java
@@ -10,43 +10,27 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
-//import akka.japi.pf.DeciderBuilder;
 import akka.pattern.Patterns;
 import akka.routing.RoundRobinPool;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import scala.concurrent.Future;
-//import scala.concurrent.duration.Duration;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
-//import static akka.actor.SupervisorStrategy.*;
-
-import ru.bmstu.akka.lab4.messages.GetMessage;
+import ru.bmstu.akka.lab4.messages.*;
 
 public class JsTesterApp extends AllDirectives {
     private static final String HOST = "http://localhost";
     private static final int PORT = 8080;
     private static final int TIMEOUT_MS = 5000;
-    private static final int MAX_RETRIES = 10;
 
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("lab4");
         ActorRef storeActor = system.actorOf(Props.create(StoreActor.class));
         ActorRef routeActor = system.actorOf(new RoundRobinPool(5)
-//                .withSupervisorStrategy(
-//                        new OneForOneStrategy(
-//                                MAX_RETRIES,
-//                                Duration.create("1 minute"),
-//                                DeciderBuilder
-//                                        .match(ArithmeticException.class, e -> resume())
-//                                        .match(NullPointerException.class, e -> restart())
-//                                        .match(IllegalArgumentException.class, e -> stop())
-//                                        .matchAny(o -> escalate()).build()
-//                        )
-//                )
                 .props(Props.create(PerformActor.class, storeActor)), "router");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
