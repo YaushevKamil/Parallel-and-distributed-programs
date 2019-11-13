b911c761572473c7d81db7114fa7b5f00c38ea15
YaushevKamil <kyaush@mail.ru>
2019-11-14 00:59:01 +0300

    Scripted auto-commit on change (2019-11-14 00:59:01) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
@@ -2,14 +2,19 @@ package ru.bmstu.akka.lab5;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class LoadTestingApp {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException {
         System.out.println("Start!");
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
@@ -19,5 +24,16 @@ public class LoadTestingApp {
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = tester
                 .createRoute(http, system, materializer)
                 .flow(system, materializer);
+
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
     }
 }
