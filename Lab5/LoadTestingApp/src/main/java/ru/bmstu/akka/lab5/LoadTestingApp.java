97052cdef0a0ebdb5f2a72c02c23f6e9ed95ed11
YaushevKamil <kyaush@mail.ru>
2019-11-14 00:57:53 +0300

    Scripted auto-commit on change (2019-11-14 00:57:53) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
@@ -17,7 +17,7 @@ public class LoadTestingApp {
         LoadTestingApp tester = new LoadTestingApp();
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = tester
-                .createRoute(http, ActorSystem, ActorMaterializer)
+                .createRoute(http, system, ActorMaterializer)
                 .flow(system, materializer);
     }
 }
