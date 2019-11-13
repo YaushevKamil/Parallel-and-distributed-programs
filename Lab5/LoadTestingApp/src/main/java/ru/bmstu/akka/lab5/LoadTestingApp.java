8533571a4bb7d4b2344897701e39492044aa577b
YaushevKamil <kyaush@mail.ru>
2019-11-14 00:52:32 +0300

    Scripted auto-commit on change (2019-11-14 00:52:32) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
@@ -1,7 +1,14 @@
 package ru.bmstu.akka.lab5;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+
 public class LoadTestingApp {
     public static void main(String[] args)  {
-
+        System.out.println("Start!");
+        ActorSystem system = ActorSystem.create("lab5");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
     }
 }
