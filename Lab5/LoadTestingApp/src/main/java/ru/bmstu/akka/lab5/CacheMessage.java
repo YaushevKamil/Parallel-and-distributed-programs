8b62858406d801adad7a45a74e3fbc65437a65a4
YaushevKamil <kyaush@mail.ru>
2019-11-14 20:32:46 +0300

    Scripted auto-commit on change (2019-11-14 20:32:46) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/CacheMessage.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/CacheMessage.java
@@ -1,4 +1,19 @@
 package ru.bmstu.akka.lab5;
 
 public class CacheMessage {
+    private String url;
+    private Integer count;
+
+    public GetMessage(String url, Integer count) {
+        this.url = url;
+        this.count = count;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
 }
