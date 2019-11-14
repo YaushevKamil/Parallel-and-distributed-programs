01f74f852be7171ccddf937473de8744e486b3d0
YaushevKamil <kyaush@mail.ru>
2019-11-14 16:17:59 +0300

    Scripted auto-commit on change (2019-11-14 16:17:58) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/CacheActor.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/CacheActor.java
@@ -2,7 +2,12 @@ package ru.bmstu.akka.lab5;
 
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CacheActor extends AbstractActor {
+    private Map<String, ArrayList<String>> store = new HashMap<>();
 
     @Override
     public Receive createReceive() {
