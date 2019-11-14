b6b16c568643ac2e3584a8866c835ba5d9857d67
YaushevKamil <kyaush@mail.ru>
2019-11-14 15:21:41 +0300

    Scripted auto-commit on change (2019-11-14 15:21:41) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
@@ -9,15 +9,23 @@ import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
+import akka.japi.Pair;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Map;
 import java.util.concurrent.CompletionStage;
 
 public class LoadTestingApp {
     private static final String HOST = "http://localhost";
     private static final int PORT = 8080;
+
+    private static final String URL_KEY = "testUrl";
+    private static final String COUNT_KEY = "count";
+
+    private static final int COUNT_ZERO = 0;
 
     public static void main(String[] args) throws IOException {
         System.out.println("Start!");
@@ -26,8 +34,7 @@ public class LoadTestingApp {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         LoadTestingApp tester = new LoadTestingApp();
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(http, system, materializer)
-                .flow(system, materializer);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createFlow(http, system, materializer);
 
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
@@ -44,7 +51,32 @@ public class LoadTestingApp {
 
     private static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(Http http, ActorSystem system, ActorMaterializer materializer) {
         return Flow.of(HttpRequest.class)
-                .map(req -> {})
-                .mapAsync(4, r -> {});
+                .map(req -> {
+                    Map<String, String> paramsMap = req.getUri().query().toMap();
+                    if (!paramsMap.containsKey(URL_KEY) || !paramsMap.containsKey(COUNT_KEY)) {
+                        System.out.println(paramsMap.toString());
+                        return new Pair<HttpRequest, Integer>(HttpRequest.create(HOST), COUNT_ZERO);
+                    }
+                    String url = paramsMap.get(URL_KEY);
+                    Integer count = Integer.parseInt(paramsMap.get(COUNT_KEY));
+                    return new Pair<HttpRequest, Integer>(HttpRequest.create(url), count);
+                })
+                .mapAsync(4, r -> {
+                    //thenCompose(res)
+                    // if (CacheActor.resPerformed)
+                    //else crea
+                    return Source.from(Collections.singletonList(r))
+                            .toMat(testSink, Keep.right()).run(materializer)
+                            .thenApply(sum -> sum/(float)((r.second() == 0)? 1:r.second()));
+                })
+                .map(res -> {
+                    System.out.println(res);
+                    // res -> CacheActor
+                    String averageTime = new DecimalFormat("#0.00").format(res);
+                    return HttpResponse
+                            .create()
+                            .withStatus(200)
+                            .withEntity("Average response time" + averageTime + " ms");
+                });
     }
 }
