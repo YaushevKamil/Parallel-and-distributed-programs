9ed29bc64f8faf58cd01594d5973e48080f89aaa
YaushevKamil <kyaush@mail.ru>
2019-11-14 17:58:02 +0300

    Scripted auto-commit on change (2019-11-14 17:58:02) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
@@ -93,7 +93,7 @@ public class LoadTestingApp {
                             .mapConcat(pair -> new ArrayList<Pair<String, Integer>>(Collections.nCopies(pair.second(), pair)))
                             .mapAsync(pair -> {
                                 // AsyncHttpClient ->
-                                CompletableFuture<Response> 
+                                CompletableFuture<Response> whenResponse
                             })
                             .toMat(fold, Keep.right());;
                     //thenCompose(res)
