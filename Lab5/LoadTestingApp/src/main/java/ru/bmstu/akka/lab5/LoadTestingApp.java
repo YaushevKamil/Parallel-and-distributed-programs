315ac44e98e2a5332db3185428661612fc6c6299
YaushevKamil <kyaush@mail.ru>
2019-11-14 21:06:52 +0300

    Scripted auto-commit on change (2019-11-14 21:06:52) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
@@ -99,7 +99,7 @@ public class LoadTestingApp {
                             TIMEOUT_MS
                     );
                     toJava(output).thenCompose(r -> {
-                        r.
+                        toJava(r).
                         if (r.calced()) {
                             return completeOKWithFuture(r.);
                         } else {
