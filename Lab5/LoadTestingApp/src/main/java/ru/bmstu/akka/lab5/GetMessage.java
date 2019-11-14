f7681f0581ae922f7e0a461db8f8ca66990eabad
YaushevKamil <kyaush@mail.ru>
2019-11-14 20:31:43 +0300

    Scripted auto-commit on change (2019-11-14 20:31:43) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/GetMessage.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/GetMessage.java
@@ -8,4 +8,12 @@ public class GetMessage {
         this.url = url;
         this.count = count;
     }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
 }
