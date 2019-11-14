8f72afa3fcb3f881869757771417f2f1394ca986
YaushevKamil <kyaush@mail.ru>
2019-11-14 20:34:56 +0300

    Scripted auto-commit on change (2019-11-14 20:34:56) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/LoadTestingApp.java
@@ -91,7 +91,7 @@ public class LoadTestingApp {
                     Integer count = pair.second();
                     Future<Object> output = Patterns.ask(
                             cacheActor,
-                            new GetMessage(, ),
+                            new GetMessage(url),
                             TIMEOUT_MS
                     );
 
