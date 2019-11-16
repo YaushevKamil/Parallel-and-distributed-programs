4dc052216bc7219889b93c0fea777e2b42e5d975
YaushevKamil <kyaush@mail.ru>
2019-11-16 21:33:10 +0300

    Scripted auto-commit on change (2019-11-16 21:33:10) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -25,7 +25,7 @@ public class Tester {
     public Flow<HttpRequest, HttpResponse, NotUsed> createFlow() {
         return Flow.of(HttpRequest.class)
                 .map(req -> {
-                    
+
                 })
                 .mapAsync()
                 .map();
