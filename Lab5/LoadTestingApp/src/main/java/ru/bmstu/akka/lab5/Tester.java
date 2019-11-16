79c8445eadb29c599e404c607fe10513e24a546c
YaushevKamil <kyaush@mail.ru>
2019-11-16 21:24:28 +0300

    Scripted auto-commit on change (2019-11-16 21:24:28) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -1,4 +1,10 @@
 package ru.bmstu.akka.lab5;
 
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import org.asynchttpclient.AsyncHttpClient;
+
 public class Tester {
+    public Tester(ActorSystem system, ActorMaterializer materializer, AsyncHttpClient asyncHttpClient) {
+    }
 }
