c27a70648552e7090e3d229ef457098e517b7b78
YaushevKamil <kyaush@mail.ru>
2019-11-16 19:53:34 +0300

    Scripted auto-commit on change (2019-11-16 19:53:34) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tests.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tests.java
@@ -4,5 +4,16 @@ public class Tests {
     private Test test; //maybe List
     private Long sum;
 
-    public Tests(Test test)
+    public Tests(Test test, Long sum) {
+        this.test = test;
+        this.sum = sum;
+    }
+
+    public Test getTest() {
+        return test;
+    }
+
+    public Long getSum() {
+        return sum;
+    }
 }
