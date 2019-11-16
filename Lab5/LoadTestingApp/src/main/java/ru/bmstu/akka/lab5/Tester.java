772359dcc875928f089bf11fe952791cee366909
YaushevKamil <kyaush@mail.ru>
2019-11-16 21:28:58 +0300

    Scripted auto-commit on change (2019-11-16 21:28:58) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -3,18 +3,21 @@ package ru.bmstu.akka.lab5;
 import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import org.asynchttpclient.AsyncHttpClient;
+import ru.bmstu.akka.lab5.Actors.CacheActor;
 
 public class Tester {
     private final ActorMaterializer materializer;
-    private final ActorRef cacheActor
+    private final ActorRef cacheActor;
 
     public Tester(ActorSystem system, ActorMaterializer materializer, AsyncHttpClient asyncHttpClient) {
         this.materializer = materializer;
+        this.cacheActor = cacheActor = system.actorOf(Props.create(CacheActor.class));
 
     }
 
