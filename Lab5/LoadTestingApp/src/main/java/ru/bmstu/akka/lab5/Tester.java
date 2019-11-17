c005984de5c86eedce1c21078dec69732c907292
YaushevKamil <kyaush@mail.ru>
2019-11-17 14:45:46 +0300

    Scripted auto-commit on change (2019-11-17 14:45:46) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -81,7 +81,7 @@ public class Tester {
         final Sink<Pair<String, Integer>, CompletionStage<Long>> testSink = createSink(test);
         return Source.from(Collections.singletonList(test))
                 .toMat(testSink, Keep.right()).run(materializer)
-                .thenApply(sum -> new StoreMessage(new GetMessage(test.first(), test.second()), ))
+                .thenApply(sum -> new StoreMessage(new GetMessage(test.first(), test.second()), sum/))
     }
 
     private Sink<Pair<String, Integer>, CompletionStage<Long>> createSink(Pair<String, Integer> test) {
