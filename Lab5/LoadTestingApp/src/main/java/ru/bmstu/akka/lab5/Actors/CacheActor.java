8cf37e05d6dc31859fc697800b7b419fd3a9054d
YaushevKamil <kyaush@mail.ru>
2019-11-17 17:12:15 +0300

    Scripted auto-commit on change (2019-11-17 17:12:15) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Actors/CacheActor.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Actors/CacheActor.java
@@ -16,13 +16,13 @@ public class CacheActor extends AbstractActor {
         return ReceiveBuilder
                 .create()
                 .match(StoreMessage.class, msg -> {
-                    System.out.println("ACTOR::" + msg.toString());
-                    System.out.println(msg.getTest().getUrl() + " " + msg.getTest().getCount() + " " + msg.getDelay());
+                    System.out.println("ACTOR::" + msg.getClass().getSimpleName());
+                    System.out.println("\t" + msg.getTest().getUrl() + " " + msg.getTest().getCount() + " " + msg.getDelay());
                     cache.put(msg.getTest(), msg.getDelay());
                 })
                 .match(GetMessage.class, msg -> {
-                    System.out.println("ACTOR::" + msg.toString());
-                    System.out.println(msg.getUrl() + " " + msg.getCount());
+                    System.out.println("ACTOR::" + msg.getClass().getSimpleName());
+                    System.out.println("\t" + msg.getUrl() + " " + msg.getCount());
                     sender()
                             .tell(new ResponseMessage(new StoreMessage(msg, cache.get(msg))),
                                     getSelf());
