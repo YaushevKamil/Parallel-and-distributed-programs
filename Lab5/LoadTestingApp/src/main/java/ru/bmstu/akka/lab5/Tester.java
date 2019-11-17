b4d5f04d2ba72aff69a4dd887c14e5ec7989bc1b
YaushevKamil <kyaush@mail.ru>
2019-11-17 13:59:32 +0300

    Scripted auto-commit on change (2019-11-17 13:59:32) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -71,7 +71,7 @@ public class Tester {
                 Patterns.ask(cacheActor, new GetMessage(url, count), TIMEOUT_MS)
                 ).thenApply(o -> (StoreMessage)o)
                 .thenCompose(resp -> {
-                    
+                    StoreMessage
                 });
     }
 }
