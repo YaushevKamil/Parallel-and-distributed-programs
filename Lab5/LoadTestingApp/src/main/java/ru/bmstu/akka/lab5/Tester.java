8c44ccd84562edb6c92878076248d30962e551f2
YaushevKamil <kyaush@mail.ru>
2019-11-17 14:34:05 +0300

    Scripted auto-commit on change (2019-11-17 14:34:05) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -93,7 +93,7 @@ public class Tester {
                 .prepareGet(url)
                 .execute()
                 .toCompletableFuture()
-                .thenCompose()
+                .thenCompose(resp -> )
     }
 
 
