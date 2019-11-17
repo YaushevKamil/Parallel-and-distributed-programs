cfa7c6b838b2b7b033d58f58e66a6fcfab54bb37
YaushevKamil <kyaush@mail.ru>
2019-11-17 17:30:29 +0300

    Scripted auto-commit on change (2019-11-17 17:30:29) by gitwatch.sh

--- Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ Lab5/LoadTestingApp/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -113,6 +113,6 @@ public class Tester {
         return HttpResponse
                 .create()
                 .withStatus(200)
-                .withEntity("Average response time" + result.getDelay() + " ms");
+                .withEntity("Average response time: " + result.getDelay() + " ms");
     }
 }
