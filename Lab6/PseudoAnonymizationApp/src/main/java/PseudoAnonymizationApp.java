91af6d86cad983c6193d11f4a25a924f1307872b
YaushevKamil <kyaush@mail.ru>
2019-11-24 01:06:48 +0300

    Scripted auto-commit on change (2019-11-24 01:06:48) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/PseudoAnonymizationApp.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/PseudoAnonymizationApp.java
@@ -1,7 +1,7 @@
 public class PseudoAnonymizationApp {
     public static void main(String[] args) {
         if (args.length < 3) {
-            System.out.println("Usage: PseudoAnonymizationApp <> <> <> <port>");
+            System.out.println("Usage: PseudoAnonymizationApp <> <> <host> <port_1> ... <port_n>");
         }
     }
 }
