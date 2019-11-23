e7a3f64d3150793ff1b689b326b182956509c0f4
YaushevKamil <kyaush@mail.ru>
2019-11-24 01:10:38 +0300

    Scripted auto-commit on change (2019-11-24 01:10:38) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/PseudoAnonymizationApp.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/PseudoAnonymizationApp.java
@@ -1,8 +1,41 @@
+import akka.actor.ActorSystem;
+
 public class PseudoAnonymizationApp {
     public static void main(String[] args) {
         if (args.length < 3) {
             System.out.println("Usage: PseudoAnonymizationApp <ZooKeeperAddr> <host> <port_1> ( ... <port_n> )");
-            
+
+
+            ActorSystem system = ActorSystem.create("routes");
+
+            final Http http = Http.get(system);
+            final ActorMaterializer materializer = ActorMaterializer.create(system);
+            final AsyncHttpClient asyncHttpClient = asyncHttpClient();
+
+            final Tester tester = new Tester(system, materializer, asyncHttpClient);
+
+            final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = tester.createFlow();
+            final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                    routeFlow,
+                    ConnectHttp.toHost(HOST, PORT),
+                    materializer
+            );
+
+            //http://localhost:8080/?testUrl=http://rambler.ru&count=20
+
+            System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+            System.in.read();
+            binding
+                    .thenCompose(ServerBinding::unbind)
+                    .thenAccept(unbound -> {
+                        system.terminate();
+                        try {
+                            asyncHttpClient.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
         }
     }
 }
