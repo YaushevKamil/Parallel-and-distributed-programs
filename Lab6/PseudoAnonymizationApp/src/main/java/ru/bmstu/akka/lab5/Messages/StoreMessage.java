b61b81874b95c4e80bd93ebdac70e58a88f14a83
YaushevKamil <kyaush@mail.ru>
2019-11-26 02:04:21 +0300

    Scripted auto-commit on change (2019-11-26 02:04:21) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Messages/StoreMessage.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Messages/StoreMessage.java
@@ -3,8 +3,8 @@ package ru.bmstu.akka.lab5.Messages;
 public class StoreMessage {
     private final String[] addresses;
 
-    public StoreMessage(String[] servers) {
-        this.addresses = servers;
+    public StoreMessage(String[] addresses) {
+        this.addresses = addresses;
     }
 
     public String[] getAddresses() {
