5fac01bfb560229ff2cc487810c54f7e4685a5ee
YaushevKamil <kyaush@mail.ru>
2019-11-26 01:37:41 +0300

    Scripted auto-commit on change (2019-11-26 01:37:41) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Actors/StoreActor.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Actors/StoreActor.java
@@ -3,6 +3,8 @@ package ru.bmstu.akka.lab5.Actors;
 import akka.actor.AbstractActor;
 
 public class StoreActor extends AbstractActor {
+    private String[] addresses;
+    
     @Override
     public Receive createReceive() {
         return null;
