ff18801ef5a30cde110ea20287a129d4be3451c1
YaushevKamil <kyaush@mail.ru>
2019-11-26 01:46:45 +0300

    Scripted auto-commit on change (2019-11-26 01:46:45) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Messages/ResponseMessage.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Messages/ResponseMessage.java
@@ -1,4 +1,13 @@
 package ru.bmstu.akka.lab5.Messages;
 
 public class ResponseMessage {
+    private final int randomNumber;
+
+    ResponseMessage(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
 }
