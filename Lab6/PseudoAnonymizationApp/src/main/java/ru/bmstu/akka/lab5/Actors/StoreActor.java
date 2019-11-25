d2f5480a78d12c79ed8bf4babc87c7bac51b6d09
YaushevKamil <kyaush@mail.ru>
2019-11-26 01:43:30 +0300

    Scripted auto-commit on change (2019-11-26 01:43:30) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Actors/StoreActor.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Actors/StoreActor.java
@@ -2,6 +2,7 @@ package ru.bmstu.akka.lab5.Actors;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import ru.bmstu.akka.lab5.Messages.GetMessage;
 import ru.bmstu.akka.lab5.Messages.StoreMessage;
 
 public class StoreActor extends AbstractActor {
@@ -11,7 +12,7 @@ public class StoreActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, msg -> servers = msg.getServers())
-                .match()
+                .match(GetMessage.class,)
                 .build();
     }
 }
