bd3f8d3ec5f64f9afe2743c75dcad2bff0e6f694
YaushevKamil <kyaush@mail.ru>
2019-11-26 01:38:51 +0300

    Scripted auto-commit on change (2019-11-26 01:38:51) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Actors/StoreActor.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Actors/StoreActor.java
@@ -9,6 +9,6 @@ public class StoreActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(StoreMessage.class, );
+                .match(StoreMessage.class, m -> );
     }
 }
