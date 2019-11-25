9558c1cdf44d86603f8b5ca6b83fe6ed9ab7fca4
YaushevKamil <kyaush@mail.ru>
2019-11-26 01:34:17 +0300

    Scripted auto-commit on change (2019-11-26 01:34:17) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/PseudoAnonymizationApp.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/PseudoAnonymizationApp.java
@@ -13,26 +13,28 @@ import java.util.concurrent.CompletionStage;
 
 public class PseudoAnonymizationApp {
     private static final String HOST = "http://localhost";
-    private static final int PORT = 8081;
+    private static final int PORT = 8080;
 
     public static void main(String[] args) throws IOException {
         if (args.length < 3) {
-            System.out.println("Usage: PseudoAnonymizationApp <ZooKeeperAddr> <host> <port_1> ( ... <port_n> )");
+            System.out.println("Usage: PseudoAnonymizationApp <host> <port_0> (<port_1> ... <port_n>)");
+            System.exit(-1);
+        }
+        String host = args[0];
+        String connectString;
 
+        ActorSystem system = ActorSystem.create("anonymizer");
 
-            ActorSystem system = ActorSystem.create("anonymizer");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Anonymizer server = new Anonymizer(system, materializer, asyncHttpClient);
 
-            final Http http = Http.get(system);
-            final ActorMaterializer materializer = ActorMaterializer.create(system);
-
-            final Tester tester = new Tester(system, materializer, asyncHttpClient);
-
-            final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = tester.createFlow();
-            final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-                    routeFlow,
-                    ConnectHttp.toHost(HOST, PORT),
-                    materializer
-            );
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = tester.createFlow();
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
 
             //http://localhost:8080/?testUrl=http://rambler.ru&count=20
 
