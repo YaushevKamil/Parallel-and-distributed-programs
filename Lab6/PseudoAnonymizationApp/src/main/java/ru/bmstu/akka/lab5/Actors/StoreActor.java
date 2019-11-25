f598bef8340b8b680c67f504cc3097c81d5ca953
YaushevKamil <kyaush@mail.ru>
2019-11-26 01:49:35 +0300

    Scripted auto-commit on change (2019-11-26 01:49:35) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Actors/StoreActor.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Actors/StoreActor.java
@@ -9,14 +9,14 @@ import ru.bmstu.akka.lab5.Messages.StoreMessage;
 import java.util.Random;
 
 public class StoreActor extends AbstractActor {
-    private String[] servers;
+    private String[] servers; // RENAME
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, msg -> servers = msg.getServers())
                 .match(GetMessage.class, msg -> {
-                    sender().tell(new ResponseMessage(new Random()))
+                    sender().tell(new ResponseMessage(new Random().nextInt(servers.length)));
                 })
                 .build();
     }
