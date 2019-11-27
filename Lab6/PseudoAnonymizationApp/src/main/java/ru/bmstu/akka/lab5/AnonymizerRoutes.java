7c4bfa0a9e7f8ad93fc826e5f3de83e430fe9e70
YaushevKamil <kyaush@mail.ru>
2019-11-27 23:37:11 +0300

    Scripted auto-commit on change (2019-11-27 23:37:11) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/AnonymizerRoutes.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/AnonymizerRoutes.java
@@ -1,4 +1,9 @@
 package ru.bmstu.akka.lab5;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
 public class AnonymizerRoutes {
+    private final ActorSystem system;
+    private final ActorRef storeActor;
 }
