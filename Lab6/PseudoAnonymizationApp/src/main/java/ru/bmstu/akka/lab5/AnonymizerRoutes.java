8e3c97a700a0289f36ab791dbfaabcb40c31a2a2
YaushevKamil <kyaush@mail.ru>
2019-11-27 23:57:58 +0300

    Scripted auto-commit on change (2019-11-27 23:57:58) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/AnonymizerRoutes.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/AnonymizerRoutes.java
@@ -6,7 +6,7 @@ import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 
 public class AnonymizerRoutes extends AllDirectives {
-    private static 
+    private static final String URL
 
     private final ActorSystem system;
     private final ActorRef storeActor;
