95ac3726e827fb8aecc7641cc59ac45a40e060b8
YaushevKamil <kyaush@mail.ru>
2019-11-28 00:21:05 +0300

    Scripted auto-commit on change (2019-11-28 00:21:05) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/AnonymizerRoutes.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/AnonymizerRoutes.java
@@ -52,6 +52,6 @@ public class AnonymizerRoutes extends AllDirectives {
     }
 
     private CompletionStage<HttpResponse> redirect(String url, int count) {
-        return Patterns.ask(storeActor, new GetMessage(), TI)
+        return Patterns.ask(storeActor, new GetMessage(), TIMEOUT_MS)
     }
 }
