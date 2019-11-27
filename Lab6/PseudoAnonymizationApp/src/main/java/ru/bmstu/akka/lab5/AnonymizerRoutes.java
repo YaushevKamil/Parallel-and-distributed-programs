27246ff2d9a638db1baff74e7e253336e6550f6d
YaushevKamil <kyaush@mail.ru>
2019-11-28 00:25:41 +0300

    Scripted auto-commit on change (2019-11-28 00:25:41) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/AnonymizerRoutes.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/AnonymizerRoutes.java
@@ -61,7 +61,8 @@ public class AnonymizerRoutes extends AllDirectives {
             ).thenApply(o -> /*(ResponseMessage)*/(String)o)
             .thenCompose(addr -> {
                 return makeRequest(Uri.create(addr).query(Query.create(
-                        Pair.create(URL_ARG_NAME)
+                        Pair.create(URL_ARG_NAME, url),
+                        
                 )))
             })
     }
