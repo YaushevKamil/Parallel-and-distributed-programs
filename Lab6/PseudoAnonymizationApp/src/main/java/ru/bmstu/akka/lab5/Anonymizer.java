4bb63923944a7097e8ab11947b73315f27e440f0
YaushevKamil <kyaush@mail.ru>
2019-11-27 23:33:53 +0300

    Scripted auto-commit on change (2019-11-27 23:33:53) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Anonymizer.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab5/Anonymizer.java
@@ -2,12 +2,13 @@ package ru.bmstu.akka.lab5;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 public class Anonymizer {
     private ActorRef storeActor;
 
     public Anonymizer(ActorSystem system, String zooKeeperHost, String host, String port) {
-        storeActor = system.actorOf(props)
+        storeActor = system.actorOf(Props.create())
 
     }
 }
