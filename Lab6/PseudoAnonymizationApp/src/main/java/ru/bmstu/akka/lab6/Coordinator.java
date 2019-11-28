a23e941ef19998c405541ba66c23b22b9921aba3
YaushevKamil <kyaush@mail.ru>
2019-11-28 13:43:17 +0300

    Scripted auto-commit on change (2019-11-28 13:43:17) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab6/Coordinator.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab6/Coordinator.java
@@ -1,13 +1,16 @@
 package ru.bmstu.akka.lab6;
 
 import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
 
 class Coordinator {
     private final String zooKeeperHost;
     private final ActorRef storeActor;
+    private final ZooKeeper zoo;
 
     Coordinator(String zooKeeperHost, ActorRef storeActor) {
         this.zooKeeperHost = zooKeeperHost;
         this.storeActor = storeActor;
+        this.zoo = ;
     }
 }
