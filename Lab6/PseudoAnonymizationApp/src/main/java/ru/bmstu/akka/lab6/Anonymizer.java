1f21b0a4e3e01bcbff97f58ad39c1a632f447ed1
YaushevKamil <kyaush@mail.ru>
2019-11-28 15:11:11 +0300

    Scripted auto-commit on change (2019-11-28 15:11:11) by gitwatch.sh

--- Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab6/Anonymizer.java
+++ Lab6/PseudoAnonymizationApp/src/main/java/ru/bmstu/akka/lab6/Anonymizer.java
@@ -5,6 +5,7 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.model.Uri;
 import akka.http.javadsl.server.Route;
+import org.apache.zookeeper.KeeperException;
 import ru.bmstu.akka.lab6.Actors.StoreActor;
 
 import java.io.IOException;
@@ -13,7 +14,7 @@ class Anonymizer {
     private final AnonymizerRoutes routes;
     private final Coordinator coordinator;
 
-    Anonymizer(ActorSystem system, String zooKeeperHost, String host, int port) throws IOException {
+    Anonymizer(ActorSystem system, String zooKeeperHost, String host, int port) throws IOException, KeeperException, InterruptedException {
         ActorRef storeActor = system.actorOf(Props.create(StoreActor.class), "HostStorage");
         String address = Uri.create("").host(host).port(port).toString();
         coordinator = new Coordinator(zooKeeperHost, storeActor, address);
