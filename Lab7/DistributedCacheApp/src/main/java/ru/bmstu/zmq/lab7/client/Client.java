1556df93afbd336f8e3b78bf3a944328a906f9de
YaushevKamil <kyaush@mail.ru>
2019-12-20 22:15:54 +0300

    Scripted auto-commit on change (2019-12-20 22:15:54) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/client/Client.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/client/Client.java
@@ -29,7 +29,7 @@ public class Client {
             Command cmd = new Command(scanner.nextLine());
             switch (cmd.getCommandType()) {
                 case GET:
-                    Integer;
+                    Integer ;
                 case PUT:
                     ;
             }
