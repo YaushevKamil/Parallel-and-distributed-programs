0ccf9653dbcd54e85bc4e501f3e61b7efb771b15
YaushevKamil <kyaush@mail.ru>
2019-12-20 20:59:01 +0300

    Scripted auto-commit on change (2019-12-20 20:59:01) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
@@ -7,6 +7,7 @@ public class Proxy {
     private static final int POLLER_SIZE = 2;
     private static final int CLIENT_POLLIN = 0;
     private static final int CACHE_POLLIN = 1;
+    
     private String clientAddress;
     private String cacheAddress;
     private ZContext context;
