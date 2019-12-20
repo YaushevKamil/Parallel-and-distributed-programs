c6ee1c05abe233904a0383a6db587193b7869b9c
YaushevKamil <kyaush@mail.ru>
2019-12-20 20:09:27 +0300

    Scripted auto-commit on change (2019-12-20 20:09:27) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/command/Command.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/command/Command.java
@@ -2,9 +2,11 @@ package ru.bmstu.zmq.lab7.command;
 
 //import java.util.regex.Pattern;
 
+import java.util.ArrayList;
+
 public class Command {
     private static final String DELIMITER = "\\s+";
-
+    private ArrayList<Integer> args;
     public enum CommandType {
         GET, PUT, NOTIFY, ERROR
     }
