0c660ae50319088a99ee762bc0ef7d0e8560eb0f
YaushevKamil <kyaush@mail.ru>
2019-12-21 00:24:07 +0300

    Scripted auto-commit on change (2019-12-21 00:24:07) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
@@ -58,7 +58,7 @@ public class Proxy {
                 switch (cmd.getCommandType()) {
                     case RESULT:
                         ZFrame clientId = msg.pop();
-                        Command 
+                        Command s
                         sendMessageToClient(clientId, new Command(Command.Type.SUCCESSFUL), cmd.getResult().toString());
                     case NOTIFY:
                         ;
