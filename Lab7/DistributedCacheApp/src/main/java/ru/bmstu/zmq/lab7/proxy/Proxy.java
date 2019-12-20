1e7135893825412230025de4acf10f44828face1
YaushevKamil <kyaush@mail.ru>
2019-12-20 17:53:37 +0300

    Scripted auto-commit on change (2019-12-20 17:53:37) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
@@ -32,7 +32,7 @@ public class Proxy {
         while(!Thread.currentThread().isInterrupted()) {
             poller.poll();
             if (poller.pollin(0)) { //client // create const
-
+                
             } else if (poller.pollin(1)) { // cache
 
             }
