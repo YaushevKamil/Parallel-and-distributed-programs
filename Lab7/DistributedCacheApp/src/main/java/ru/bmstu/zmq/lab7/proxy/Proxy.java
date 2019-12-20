fa2d37a2509928ccf1a98e4aedaa013e3adf2aa4
YaushevKamil <kyaush@mail.ru>
2019-12-20 17:42:47 +0300

    Scripted auto-commit on change (2019-12-20 17:42:47) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
@@ -1,12 +1,13 @@
 package ru.bmstu.zmq.lab7.proxy;
 
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
 public class Proxy {
     private final String clientAddress;
     private final String cacheAddress;
     private ZContext context;
-    private 
+    private ZMQ.Socket
 
     public Proxy(ZContext context, String clientAddress, String cacheAddress) {
         this.clientAddress = clientAddress;
