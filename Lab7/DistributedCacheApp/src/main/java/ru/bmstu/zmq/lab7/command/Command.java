0d0c169c175c105024cbe11332ecd5f3a72ae8d4
YaushevKamil <kyaush@mail.ru>
2019-12-20 20:29:50 +0300

    Scripted auto-commit on change (2019-12-20 20:29:50) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/command/Command.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/command/Command.java
@@ -13,7 +13,7 @@ public class Command {
     private static final String DELIMITER = "\\s+";
     private ArrayList<Integer> args;
     private CommandType type;
-    private
+    private Pattern 
 
     public Command(String raw) {
 
