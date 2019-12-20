0aed1a4c85839f2928f1890b04e75906e6f2d5d8
YaushevKamil <kyaush@mail.ru>
2019-12-20 23:38:46 +0300

    Scripted auto-commit on change (2019-12-20 23:38:46) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
@@ -71,7 +71,7 @@ public class Proxy {
         msg.add(clientId);
         msg.add(cmd.toString());
         System.out.println("Message to cache: " + msg);
-        
+        msg.s
     }
 
     public void terminate() {
