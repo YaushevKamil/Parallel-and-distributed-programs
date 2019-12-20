cde9dec6c006ce96c1ed587eadeda9b011e6615d
YaushevKamil <kyaush@mail.ru>
2019-12-20 21:36:19 +0300

    Scripted auto-commit on change (2019-12-20 21:36:19) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/client/ClientApp.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/client/ClientApp.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 public class ClientApp {
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.out.println("Usage: ClientApp
+            System.out.println("Usage: ClientApp <clientAddress>");
+            
         }
         String clientAddress = args[1];
         Scanner scanner = new Scanner(System.in);
