2661cf376eeb510a867f21760e6e7ac75a953909
YaushevKamil <kyaush@mail.ru>
2019-12-20 23:46:05 +0300

    Scripted auto-commit on change (2019-12-20 23:46:05) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
@@ -65,9 +65,9 @@ public class Proxy {
         }
     }
 
-    private void sendMessageToClient(ZFrame clientId, Command cmd) {
+    private void sendMessageToClient(ZFrame clientId, String result) {
         ZMsg msg = new ZMsg();
-
+        
     }
 
     private void sendMessageToCache(ZFrame cacheId, ZFrame clientId, Command cmd) {
