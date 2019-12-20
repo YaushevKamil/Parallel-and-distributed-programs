f823fdc8f613e115c7cdfb1f793112223fe7aa8e
YaushevKamil <kyaush@mail.ru>
2019-12-20 17:36:33 +0300

    Scripted auto-commit on change (2019-12-20 17:36:33) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/ProxyApp.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/ProxyApp.java
@@ -2,9 +2,11 @@ package ru.bmstu.zmq.lab7.proxy;
 
 import org.zeromq.ZContext;
 
+import java.net.Proxy;
+
 public class ProxyApp {
     public static void main(String[] args) {
         ZContext context = new ZContext();
-        
+        Proxy 
     }
 }
