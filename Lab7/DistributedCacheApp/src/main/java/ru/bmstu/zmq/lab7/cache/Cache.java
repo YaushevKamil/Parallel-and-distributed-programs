2bc62432520e41e402b553dd3d92b97c6271cc80
YaushevKamil <kyaush@mail.ru>
2019-12-21 18:37:34 +0300

    Scripted auto-commit on change (2019-12-21 18:37:34) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/Cache.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/Cache.java
@@ -3,6 +3,8 @@ package ru.bmstu.zmq.lab7.cache;
 import org.zeromq.ZContext;
 
 public class Cache {
+    private String addtess;
+    
     public Cache(ZContext context, String address, int leftBound, int rightBound, int initialValue) {
 
     }
