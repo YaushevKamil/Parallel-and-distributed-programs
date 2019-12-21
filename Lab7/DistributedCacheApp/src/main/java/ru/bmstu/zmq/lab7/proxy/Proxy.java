b708fa99a2f029f49e3fbde1238fca976ca707dc
YaushevKamil <kyaush@mail.ru>
2019-12-22 02:23:33 +0300

    Scripted auto-commit on change (2019-12-22 02:23:33) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
@@ -48,7 +48,7 @@ public class Proxy {
                 switch (cmd.getCommandType()) {
                     case GET:
                         Optional<ZFrame> storageId = activeStorages.getStorageId(cmd.getIndex());
-                        storageId.isPresent()
+//                        storageId.isPresent()
                         storageId.ifPresent(sendMessageToCache(storageId.get(), clientId, cmd));
                     case PUT:
                         ;
