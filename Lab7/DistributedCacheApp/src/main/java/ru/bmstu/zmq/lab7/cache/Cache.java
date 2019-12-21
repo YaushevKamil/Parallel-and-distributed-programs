4eb4da607e82489682437d8dc4a289c175e3ddbb
YaushevKamil <kyaush@mail.ru>
2019-12-21 18:52:03 +0300

    Scripted auto-commit on change (2019-12-21 18:52:03) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/Cache.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/Cache.java
@@ -6,7 +6,7 @@ public class Cache {
     private String address; // ?????????
     private Storage storage;
     private ZContext context;
-    
+    private 
 
     public Cache(ZContext context, String address, int leftBound, int rightBound, int initialValue) {
 
