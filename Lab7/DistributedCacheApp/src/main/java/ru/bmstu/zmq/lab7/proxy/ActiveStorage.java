4d97d51f39cb2124ab7e82165ce82211b389b241
YaushevKamil <kyaush@mail.ru>
2019-12-22 02:04:23 +0300

    Scripted auto-commit on change (2019-12-22 02:04:23) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/ActiveStorage.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/ActiveStorage.java
@@ -8,45 +8,6 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ActiveStorage {
-    private static class StorageInfo {
-        private static final int NOTIFY_DURATION_MS = 500;
-
-        ZFrame storageId;
-        int firstIndex;
-        int lastIndex;
-        long lastNotifyTime;
-
-        public StorageInfo(ZFrame storageId, int firstIndex, int lastIndex) {
-            this.storageId = storageId;
-            this.firstIndex = firstIndex;
-            this.lastIndex = lastIndex;
-//            lastNotifyTime = System.currentTimeMillis();
-            updateNotifyTime();
-        }
-
-        public boolean isIndexInside(int index) {
-            return firstIndex <= index && index <= lastIndex;
-        }
-
-        public void updateNotifyTime() { // f() => f(long newTimeValue)!!!
-            lastNotifyTime = System.currentTimeMillis();
-        }
-
-        public boolean isAlive() {
-            long currentTime = System.currentTimeMillis();
-            return (currentTime - lastNotifyTime) < 2 * NOTIFY_DURATION_MS;
-        }
-
-        public ZFrame getStorageId() {
-            return storageId;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            return (o instanceof StorageInfo) && this.storageId == ((StorageInfo) o).storageId;
-        }
-    }
-
     private List<StorageInfo> storages;
 
     public  ActiveStorage() {
--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/StorageInfo.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/StorageInfo.java
@@ -1,4 +1,42 @@
 package ru.bmstu.zmq.lab7.proxy;
 
+import org.zeromq.ZFrame;
+
 public class StorageInfo {
+    private static final int NOTIFY_DURATION_MS = 500;
+
+    ZFrame storageId;
+    int firstIndex;
+    int lastIndex;
+    long lastNotifyTime;
+
+    public StorageInfo(ZFrame storageId, int firstIndex, int lastIndex) {
+        this.storageId = storageId;
+        this.firstIndex = firstIndex;
+        this.lastIndex = lastIndex;
+//            lastNotifyTime = System.currentTimeMillis();
+        updateNotifyTime();
+    }
+
+    public boolean isIndexInside(int index) {
+        return firstIndex <= index && index <= lastIndex;
+    }
+
+    public void updateNotifyTime() { // f() => f(long newTimeValue)!!!
+        lastNotifyTime = System.currentTimeMillis();
+    }
+
+    public boolean isAlive() {
+        long currentTime = System.currentTimeMillis();
+        return (currentTime - lastNotifyTime) < 2 * NOTIFY_DURATION_MS;
+    }
+
+    public ZFrame getStorageId() {
+        return storageId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return (o instanceof StorageInfo) && this.storageId == ((StorageInfo) o).storageId;
+    }
 }
