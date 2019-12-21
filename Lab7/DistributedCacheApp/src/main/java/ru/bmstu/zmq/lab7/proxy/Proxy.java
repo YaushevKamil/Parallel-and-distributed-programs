519f083cf3f9edd05218e30ff57debfbfdbacd3b
YaushevKamil <kyaush@mail.ru>
2019-12-22 02:28:16 +0300

    Scripted auto-commit on change (2019-12-22 02:28:16) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/proxy/Proxy.java
@@ -55,7 +55,7 @@ public class Proxy {
                             sendMessageToClient(clientId, "ERROR");
                         }
                     case PUT:
-                        List<ZFrame> ;
+                        List<ZFrame> storagesId = ;
                 }
             } else if (poller.pollin(CACHE_POLL)) {
                 ZMsg msg = ZMsg.recvMsg(cacheRouter);
