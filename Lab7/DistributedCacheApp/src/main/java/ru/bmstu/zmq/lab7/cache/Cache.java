31c9642f9e11e9c0f545462da054c0cb12982c56
YaushevKamil <kyaush@mail.ru>
2019-12-21 21:17:53 +0300

    Scripted auto-commit on change (2019-12-21 21:17:53) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/Cache.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/Cache.java
@@ -45,7 +45,7 @@ public class Cache {
             poller.poll(nextNotifyTime - currentTime);
             if (poller.pollin(PROXY_POLL)) {
                 ZMsg msg = ZMsg.recvMsg(dealer);
-                
+                System.out.println("Message from proxy: " + (nextNotifyTime - currentTime));
                 Command cmd = new Command();
             }
         }
