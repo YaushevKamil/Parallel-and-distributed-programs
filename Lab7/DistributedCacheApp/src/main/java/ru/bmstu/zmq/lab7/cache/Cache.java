d9e5efaef05c549d02c10f1e99d217c718c5406b
YaushevKamil <kyaush@mail.ru>
2019-12-21 21:21:53 +0300

    Scripted auto-commit on change (2019-12-21 21:21:53) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/Cache.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/Cache.java
@@ -48,7 +48,7 @@ public class Cache {
                 switch (cmd.getCommandType()) {
                     case GET:
                         ZMsg reply = new ZMsg();
-                        reply.add(new Command(Command.Type.RESULT, storage.get()))
+                        reply.add(new Command(Command.Type.RESULT, storage.get(cmd.)))
                 }
             }
         }
