4c090648ccc08152c1ae0476a63731b7947fade3
YaushevKamil <kyaush@mail.ru>
2019-12-21 18:22:50 +0300

    Scripted auto-commit on change (2019-12-21 18:22:50) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/CacheApp.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/CacheApp.java
@@ -2,7 +2,7 @@ package ru.bmstu.zmq.lab7.cache;
 
 public class CacheApp {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length != 3) {
             System.out.println("Usage: CacheApp <clientAddress>");
             System.exit(-1);
         }
