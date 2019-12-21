8c2c02e61d96023ce4e39998a8584ffe31f2769b
YaushevKamil <kyaush@mail.ru>
2019-12-21 18:28:25 +0300

    Scripted auto-commit on change (2019-12-21 18:28:24) by gitwatch.sh

--- Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/CacheApp.java
+++ Lab7/DistributedCacheApp/src/main/java/ru/bmstu/zmq/lab7/cache/CacheApp.java
@@ -1,14 +1,27 @@
 package ru.bmstu.zmq.lab7.cache;
 
+import ru.bmstu.zmq.lab7.command.Command;
+
 public class CacheApp {
     public static void main(String[] args) {
         if (args.length != 3) {
             usage();
             System.exit(-1);
         }
+
     }
 
     private static void usage() {
         System.out.println("Usage: CacheApp <leftBound> <rightBound> <initialValue>");
+    }
+
+    private boolean isNumeric(String numString) {
+        return numString != null && numPattern.matcher(numString).matches();
+    }
+
+    private int strToInt(String numString) {
+        return isNumeric(numString) ?
+                Integer.parseInt(numString) :
+                INT_ZERO;
     }
 }
